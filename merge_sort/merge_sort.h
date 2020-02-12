4e554af625517f35a7512c6dc5bc6ff18c021585
bekr1024 <benjamin.david.krug@gmail.com>
2020-02-12 16:49:22 -0700

    added merge_sort directory in order to practice adding merge_sort functionality in various languages and data types.

---
+++ merge_sort/merge_sort.h
@@ -0,0 +1,13 @@
+#ifndef MERGE_SORT_H
+#define MERGE_SORT_H
+
+#include <array>
+#include <iostream>
+
+void merge_sort(int *arr, int startpoint, int endpoint);
+void merge_arrays(int *arr, int startpoint, int midpoint, int endpoint);
+void print_array(int *arr, int arr_size);
+
+
+
+#endif // MERGE_SORT_H
