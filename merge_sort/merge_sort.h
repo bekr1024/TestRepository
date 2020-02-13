03de9132d1d6c245a47fbbdd80a424d28baf0427
bekr1024 <benjamin.david.krug@gmail.com>
2020-02-12 17:57:22 -0700

    updated merge_sort to include long and float types as well.

--- merge_sort/merge_sort.h
+++ merge_sort/merge_sort.h
@@ -4,9 +4,18 @@
 #include <array>
 #include <iostream>
 
+
+void merge_sort(long *arr, int startpoint, int endpoint);
+void merge_arrays(long *arr, int startpoint, int midpoint, int endpoint);
+void print_array(long *arr, int arr_size);
+
 void merge_sort(int *arr, int startpoint, int endpoint);
 void merge_arrays(int *arr, int startpoint, int midpoint, int endpoint);
 void print_array(int *arr, int arr_size);
+
+void merge_sort(float *arr, int startpoint, int endpoint);
+void merge_arrays(float *arr, int startpoint, int midpoint, int endpoint);
+void print_array(float *arr, int arr_size);
 
 
 
